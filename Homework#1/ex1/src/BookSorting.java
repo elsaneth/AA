164593405fb8796a182978cbc05158ca7bba6a24
elsaneth <elisabeth.suits@gmail.com>
2023-10-03 23:52:46 +0300

    bookSorting update

--- Homework#1/ex1/src/BookSorting.java
+++ Homework#1/ex1/src/BookSorting.java
@@ -1,36 +1,28 @@
-public class bookSorting {
-    static void bubbleSort(int[] arr) {
+public class BookSorting {
+    static int[] bubbleSort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(i);
-            for (int k : arr) {
-                System.out.print(k + " ");
-            }
             for (int j = 1; j < (arr.length - i); j++) {
-                System.out.println("Mitmes " + j);
                 if (arr[j - 1] > arr[j]) {
                     int temp = arr[j - 1];
                     arr[j - 1] = arr[j];
                     arr[j] = temp;
                 }
-                for (int k : arr) {
-                    System.out.print(k + " ");
-                }
             }
         }
+        return arr;
     }
     public static void main(String[] args) {
-        int[] arr ={3,60,35,2,45,320,5};
+        int[] bookHeightList ={10, 15, 14, 23, 12};
 
         System.out.println("Array Before Bubble Sort");
-        for (int j : arr) {
+        for (int j : bookHeightList) {
             System.out.print(j + " ");
         }
         System.out.println();
 
-        bubbleSort(arr);//sorting array elements using bubbˆRle sort
-
+        int[] sortedBookList = bubbleSort(bookHeightList);
         System.out.println("Array After Bubble Sort");
-        for (int j : arr) {
+        for (int j : sortedBookList) {
             System.out.print(j + " ");
         }
     }
